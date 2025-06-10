4bde76ea32d5d45cac2afc9c27ddf632deaf19a8
melsikdavtan <melsik.davtyan@bk.ru>
2025-06-10 18:53:35 +0300

    Melsik123

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -3,10 +3,15 @@ import org.springframework.context.annotation.AnnotationConfigApplicationContext
 
 public class App {
     public static void main(String[] args) {
-        ApplicationContext applicationContext =
-                new AnnotationConfigApplicationContext(AppConfig.class);
-        HelloWorld bean =
-                (HelloWorld) applicationContext.getBean("helloworld");
-        System.out.println(bean.getMessage());
+        ApplicationContext context = new AnnotationConfigApplicationContext(AppConfig.class);
+
+        HelloWorld bean1 = (HelloWorld) context.getBean("helloWorld");
+        HelloWorld bean2 = (HelloWorld) context.getBean("helloWorld");
+
+        Cat cat1 = (Cat) context.getBean("cat");
+        Cat cat2 = (Cat) context.getBean("cat");
+
+        System.out.println("HelloWorld beans equal: " + (bean1 == bean2)); // true
+        System.out.println("Cat beans equal: " + (cat1 == cat2));           // false
     }
 }
---
+++ src/main/java/Cat.java
@@ -0,0 +1,5 @@
+public class Cat {
+    public void say(){
+        System.out.println("cat say");
+    }
+}
